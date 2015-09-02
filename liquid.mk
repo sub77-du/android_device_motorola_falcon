3fddc88533a21c85471e50a8061e9522a7605d5e
srisurya95 <srisurya95@gmail.com>
2015-09-02 10:33:48 +0000

    Liquify
    Change-Id: I736e87df9d1338f8530f0e47aca462be46baf7bd

--- liquid.mk
+++ liquid.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := cm_falcon
+PRODUCT_NAME := liquid_falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
