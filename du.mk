9da8761087948e5c9838bd90c75201b9b1176197
srisurya95 <srisurya95@gmail.com>
2016-03-18 12:33:10 +0530

    Add du.mk
    Change-Id: I3582febe654d1c58117c5f8c9a7bc897efa02d02

---
+++ du.mk
@@ -0,0 +1,9 @@
+$(call inherit-product, device/motorola/falcon/full_falcon.mk)
+
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+
+PRODUCT_RELEASE_NAME := MOTO G
+PRODUCT_NAME := du_falcon
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
